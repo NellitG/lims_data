160e282cbc3bc005b436904fe24f648aeb78c6f5
NellitG <kamaunelson157@gmail.com>
2026-03-13 10:50:13 +0300

    register muranga app for data extraction, create management, command folder and extracted data for farmers to database

--- extract/settings.py
+++ extract/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'transnzoia',
     'tharaka',
     'nyeri',
+    'muranga',
 ]
 
 MIDDLEWARE = [
---
+++ muranga/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MurangaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'muranga'
