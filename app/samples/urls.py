9d9cd99518bb8232f5c35a62edfca2acc6527a7e
NellitG <kamaunelson157@gmail.com>
2026-06-10 11:27:32 +0300

    Add path to list all samples

--- app/samples/urls.py
+++ app/samples/urls.py
@@ -3,6 +3,6 @@ from .views import get_sample_by_id, list_samples
 
 urlpatterns = [
     # List sample
-    path('sample/', list_samples),
+    path('samples/', list_samples),
     path('sample/<str:sample_id>/', get_sample_by_id),
 ]
