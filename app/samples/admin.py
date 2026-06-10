31607b4866729826e2d7c83c6c9556981602b454
NellitG <kamaunelson157@gmail.com>
2026-06-10 11:39:50 +0300

    create admin interface to display all the soil samples data

--- app/samples/admin.py
+++ app/samples/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import SoilSample
 
-# Register your models here.
+class SoilSampleAdmin(admin.ModelAdmin):
+    list_display = ('sample_id', 'client_name', 'town', 'county', 'date_sample_received')
+    # search_fields = ('sample_id', 'client_name', 'town', 'county')
+    # list_filter = ('county', 'date_sample_received')
+
+admin.site.register(SoilSample, SoilSampleAdmin)
